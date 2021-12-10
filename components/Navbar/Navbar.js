40af4d791bb65c34565a6f291b86ae1c2aaee10f
Pablo Restrepo M <pablorestrepom757@gmail.com>
2021-12-10 09:02:27 -0500

    CSS styles for Navbar component

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,22 +1,43 @@
 import styles from './Navbar.module.scss'
 import Image from 'next/image'
+import Link from 'next/link'
 
 import icon from '../../public/Navbar-icon.svg'
-import line from '../../public/Navbar-line.svg'
 import phone from '../../public/Navbar-phone.svg'
 
 export default function Navbar(){
     return(
         <div className={styles.root}>
-        <Image src={icon} alt='Company icon'/>
-        <a>Our services</a>
-        <a>About</a>
-        <a>Blog&News</a>
-        <a>Contact</a>
-        <a>Acount</a>
-        <Image src={line} alt='line'/>
-        <Image src={phone} alt='phone icon'/>
-        <p>+2349067322844</p>
-       </div>
+            <div className={styles.iconContainer}>
+                <Image src={icon} alt='Company icon'/>
+            </div>
+            <div className={styles.primaryLinkContainer}>
+                <Link href='#'>
+                    <a className={styles.linkUnderline}>Our Services</a>
+                </Link>
+            </div>
+            <div className={styles.secondaryLinksContainer}>
+                <Link href='#'>
+                    <a className={styles.linkUnderline}>About</a>
+                </Link>
+                <Link href='#'>
+                    <a className={styles.linkUnderline}>Blog&News</a>
+                </Link>
+                <Link href='#'>
+                    <a className={styles.linkUnderline}>Contact</a>
+                </Link>
+            </div>
+            <div className={styles.tertiaryLinkContainer}>
+                <Link href='#'>
+                    <a className={styles.linkUnderline}>Account</a>
+                </Link>
+            </div>
+            <div className={styles.phoneContainer}>
+                <a href='tel:+2349067322844' className={styles.phoneLink}>
+                    <Image className={styles.phoneIcon} layout='fixed' src={phone} alt='phone icon'/>
+                    +2349067322844
+                </a>
+            </div>
+        </div>
     )
 }
