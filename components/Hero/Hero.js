91c9fa6057713cbbc1dca148becb85797454b6ae
Pablo Restrepo M <pablorestrepom757@gmail.com>
2021-12-13 14:55:46 -0500

    Addin the Hero slides data to show, and refactorin Hero to map all the slides

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -2,19 +2,17 @@ import PropTypes from 'prop-types';
 import HeroSlide, {shape} from './HeroSlide';
 
 export default function Hero(props) {
-    return <HeroSlide 
-        menuText={props.slides[0].menuText}
-        mainText={props.slides[0].mainText}
-        secondaryText={props.slides[0].secondaryText}
-        image={props.slides[0].image}
-        imageAlt={props.slides[0].imageAlt}
-    >
-        {props.slides[0].children}
-    </HeroSlide>
+    return <>
+        {props.slides.map((slide)=>{
+            return <HeroSlide key={slide.mainText} {...slide} />
+        })}
+    </>
 }
 
 Hero.propTypes = {
     slides: PropTypes.arrayOf(
         PropTypes.shape(shape)
-    )
-}
+        )
+    }
+    
+    
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,8 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.scss'
-import Navbar from '../components/navbar/Navbar'
-import Hero from '../components/Hero/Hero'
-import Button from '../components/Button/Button'
-import womanInAComputerImage from '../public/womanInAComputerImage.jpg'
+import Head from 'next/head';
+import styles from '../styles/Home.module.scss';
+import Navbar from '../components/navbar/Navbar';
+import Hero from '../components/Hero/Hero';
+import data from '../data/slidesData'
 
 
 export default function Home() {
@@ -16,19 +15,7 @@ export default function Home() {
       </Head>
       <Navbar/>
       <Hero
-        slides={[
-          {
-            menuText:"Hosting",
-            mainText:"Premium Web Hosting for Your Website",
-            secondaryText:"Blazing fast web hosting for individuals and businesses of all sizes backed by 24x7x365 Support.",
-            children: <>
-              <Button secondary href="#" className={styles.createAccountButton}>Create an Account</Button>
-              <Button href="#">Choose your plan</Button>
-            </>,
-            image: womanInAComputerImage,
-            imageAlt: "Woman in a computer"
-          }
-        ]}
+        slides={data}
       />
     </div>
   )
