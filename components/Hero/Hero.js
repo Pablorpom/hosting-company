da370f1f8e79db83544d643185b287ec7242bce1
Pablo Restrepo M <pablorestrepom757@gmail.com>
2022-01-14 09:05:23 -0500

    Creating the controls and the styles for the indicator lines

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -1,16 +1,22 @@
 import PropTypes from 'prop-types';
 import HeroSlide, {shape} from './HeroSlide';
+import HeroControls from './HeroControls';
 import { useState } from 'react';
 import styles from './Hero.module.scss'
 
 export default function Hero(props) {
     const [currentSlide, setCurrentSlide] = useState(0);
-    
     return <div className={styles.root}>
-        {props.slides.map((slide, index)=>{
-            const showHide = !(index === currentSlide)
-            return <HeroSlide hide={showHide} className={styles.slide} key={slide.mainText} {...slide} />
-        })}
+        <div>
+            <HeroControls data={props.slides} onClick={setCurrentSlide} index={currentSlide} />
+        </div>
+        <div>
+            {props.slides.map((slide, index)=>{
+                const showHide = !(index === currentSlide)
+                return <HeroSlide hide={showHide} className={styles.slide} key={slide.mainText} {...slide} />
+            })}
+        </div>
+        
     </div>
 }
 
---
+++ components/Hero/HeroControls.js
@@ -0,0 +1,41 @@
+import PropTypes from 'prop-types';
+import { useLayoutEffect, useRef } from 'react';
+import styles from './HeroControls.module.scss'
+import {shape} from '../Hero/HeroSlide'
+
+export default function HeroControls(props) {
+    const rootRef = useRef();
+    const indicatorContainerRef = useRef();
+    const indicatorRef = useRef();
+    useLayoutEffect(()=>{
+        const allButtons = rootRef.current.querySelectorAll('button');
+        const currentButton = allButtons[props.index];
+        const buttonWidth = currentButton.offsetWidth;
+        indicatorRef.current.style.width =  `${buttonWidth * 0.85}px`;
+        const rootClientRect = rootRef.current.getBoundingClientRect();
+        const currentButtonClientRect = currentButton.getBoundingClientRect();
+        const offset = currentButtonClientRect.left - rootClientRect.left;
+        indicatorRef.current.style.left = `${offset + ((buttonWidth * 0.15)/2)}px `;
+    },[props.index])
+    return <div ref={rootRef} className={styles.root}>
+    <div className={styles.optionsList}>
+    {props.data.map((slide, index)=>{
+        const onClick = ()=>{
+            props.onClick(index);
+        }
+        const activeClass = index === props.index ? styles.active : ''; 
+        const className = `${styles.button} ${activeClass}`;
+        
+        return <button className={className} onClick={onClick} key={slide.menuText}>{slide.menuText}</button>
+    })}
+    </div>
+    <div ref={indicatorContainerRef} className={styles.indicatorContainer}>
+        <div ref={indicatorRef} className={styles.indicator}></div>
+    </div>
+    </div>
+}
+
+HeroControls.propTypes = {
+    data: PropTypes.arrayOf(PropTypes.shape(shape)),
+    onClick: PropTypes.func
+}
